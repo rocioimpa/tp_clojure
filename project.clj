4877613f42475989be3deeaee9ab3f1d5a12644c
rocioimpa <rocio.impaglione@gmail.com>
2020-07-06 21:21:04 -0300

    Agrego server que devuelve la hora en puerto 8080

--- project.clj
+++ project.clj
@@ -1,9 +1,5 @@
 (defproject tp_clojure "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
-  :main ^:skip-aot tp-clojure.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :description "HTTP Server"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"}
+  :dependencies [[org.clojure/clojure "1.10.1"] [http-kit "2.2.0"] [clj-time "0.14.0"]]
+  :main tp-clojure.core)
--- src/tp_clojure/core.clj
+++ src/tp_clojure/core.clj
@@ -1,7 +1,15 @@
 (ns tp-clojure.core
-  (:gen-class))
+  (:require [org.httpkit.server :refer [run-server]]
+            [clj-time.core :as t]))
 
-(defn -main
+(defn app [req]
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    (str (t/time-now))})
+
+(defn -main 
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Hello, World!")
+  (run-server app {:port 8080})
+  (println "Server started at port 8080"))
