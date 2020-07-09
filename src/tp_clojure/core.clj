83493f1b5104a08cceb7e80e5ba4b397add853a0
Jazmin Ferreiro <jazminsofiaf@gmail.com>
2020-07-09 19:22:01 -0300

    all works

--- src/tp_clojure/core.clj
+++ src/tp_clojure/core.clj
@@ -1,29 +1,20 @@
 (ns tp-clojure.core
-  (:gen-class)
-  (:require [clojure-csv.core :as csvcore]
-            [clojure.data.csv :as csv]
-            [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io]
+            [clojure.data.csv :as csv]))
 
-;(defn app [req]
-; {:status  200
-; :headers {"Content-Type" "text/html"}
-;:body    (str (t/time-now))})
+(def test-csv "dataset.csv")
 
-(defn process-csv [file]
-  (with-open [in-file (clojure.java.io/reader file)]
+(defn test-read []
+  (with-open [reader (io/reader (io/resource test-csv))]
     (doall
-     (clojure.data.csv/read-csv in-file))))
+     (csv/read-csv reader)))
+  )
+
+(defn -main [& args]
+  (println (test-read)))
 
 
-(defn take-csv
-  [file]
-  (with-open  [rdr  (io/reader file)]
-    (csvcore/parse-csv rdr)))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [&]
-  (println "Hello, World!")
-  (println (process-csv "./data/survey_results_public.csv"))
-;(run-server app {:port 8080})
-  (println "Server started at port 8080"))
+
+
+
