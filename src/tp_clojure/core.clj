82fc66cd23ef41ab09a3f96cbb051633d03c178f
Jazmin Ferreiro <jazminsofiaf@gmail.com>
2020-07-12 16:55:40 -0300

    into dictionaries

--- src/tp_clojure/core.clj
+++ src/tp_clojure/core.clj
@@ -1,17 +1,33 @@
 (ns tp-clojure.core
   (:require [clojure.java.io :as io]
-            [clojure.data.csv :as csv]))
+            [clojure.data.csv :as csv]
+            [mikera.vectorz.core :as v]))
 
-(def test-csv "dataset.csv")
 
-(defn test-read []
-  (with-open [reader (io/reader (io/resource test-csv))]
+
+
+
+;function that take all csv in memory
+(defn read-all-dataset [arg]
+  (with-open [reader (io/reader (io/resource arg))]
     (doall
      (csv/read-csv reader)))
   )
 
+;function that read data from csv (already readed) and transform in file in dictionary
+(defn csv-data->maps [csv-data]
+  (map zipmap
+       (repeat (map keyword (first csv-data)))
+       (rest csv-data))
+  )
+
+(def filename "dataset.csv")
+(def csvdata (read-all-dataset filename))
+(def dataset (csv-data->maps csvdata))
+
 (defn -main [& args]
-  (println (test-read)))
+  (run! println dataset)
+  )
 
 
 
