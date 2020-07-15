b8e4b34a8023f4b827d3d32c5a63a5aa028809a9
rocioimpa <rocio.impaglione@gmail.com>
2020-07-14 23:10:07 -0300

    WIP (parser not storing correctly)

--- src/tp_clojure/core.clj
+++ src/tp_clojure/core.clj
@@ -2,6 +2,11 @@
   (:require [clojure-csv.core :as csv])
   (:require [clojure.java.io :as io]))
 
+  
+
+  (def movie-durations ())
+  ;(def average-duration (promise))
+
   (defn split [sep s]
     (clojure.string/split s sep))
 
@@ -14,13 +19,18 @@
     (let [lines (lines sep contents)
           cols (first lines)
           rows (rest lines)]
-        (print lines)
+        ;(print lines) ;uncomment this to print the lines
       (map (partial zipmap cols) rows)))
 
 (defn read-csv [filepath]
   (let [contents (slurp filepath)]
     (maps #"," contents)))
 
+(def filepath "resources/dataset.csv")    
+(def stored-data (read-csv filepath))  
+(def names (get stored-data "title"))
+
 (defn -main [& args]
-  (read-csv "resources/netflix_titles.csv")
+  (println stored-data)
+;  (println (deref average-duration))
 )
