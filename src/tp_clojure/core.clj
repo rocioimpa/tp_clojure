f332479d2e8df5c22adfc0b314e86d66ac101687
rocioimpa <rocio.impaglione@gmail.com>
2020-07-20 00:29:30 -0300

    Calculated runtime average

--- src/tp_clojure/core.clj
+++ src/tp_clojure/core.clj
@@ -1,20 +1,19 @@
  (ns tp-clojure.core
   (:require [clojure-csv.core :as csv])
   (:require [clojure.java.io :as io]))
-
-
-
-  (def movie-durations ())
-  (def average-duration (promise))
+ 
+  (defn calculate-average-runtime [total-sum, amount]
+    (double (/ total-sum amount)))
 
   (defn filterGenre [map]
   (filter (fn [entry] (= (get entry ":genre") "Adventure")) map))
 
-  (defn sumaRuntime [mapa]
-    (->> mapa
-         (map :runtime)
-         (reduce +))
-    )
+  (defn parse-int [s]
+    (Integer. (re-find  #"\d+" s )))
+
+  (defn sum-runtime [mapa]
+    (reduce + (map (fn [entry] (+ (parse-int (get entry ":runtime")))) mapa)))
+
   (defn split [sep s]
     (clojure.string/split s sep))
 
@@ -38,8 +37,10 @@
 (def stored-data (read-csv filepath))
 (def names (get stored-data "title"))
 (def resultado (filterGenre stored-data))
+(def movies-amount (count stored-data))
+(def total-sum (sum-runtime stored-data))
+(def average-runtime (calculate-average-runtime total-sum movies-amount))
 
 (defn -main [& args]
-  (println stored-data)
-  (println resultado)
+  (println average-runtime)
 )
