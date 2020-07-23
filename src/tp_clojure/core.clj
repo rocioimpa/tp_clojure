337081c13958019346561cdd16eb7bcf9d88d750
Luciano <lucianortiz97@gmail.com>
2020-07-22 23:14:07 -0300

    Parseo de tipos a cada columna

--- src/tp_clojure/core.clj
+++ src/tp_clojure/core.clj
@@ -12,10 +12,10 @@
     (Integer. (re-find  #"\d+" s )))
 
   (defn sum-runtime [mapa]
-    (reduce + (map (fn [entry] (+ (parse-int (get entry ":runtime")))) mapa)))
+    (reduce + (map (fn [entry] (+ (get entry ":runtime"))) mapa)))
 
 (defn calculate-max-votes [mapa]
- (reduce max (map (fn [entry] (+ (parse-int (get entry ":votes")))) mapa)))
+ (reduce max (map (fn [entry] (+ (get entry ":votes"))) mapa)))
 
   (defn split [sep s]
     (clojure.string/split s sep))
@@ -32,12 +32,28 @@
         ;(print lines) ;uncomment this to print the lines
       (map (partial zipmap cols) rows)))
 
+(defn parser-tipos [data]
+  (map (fn [fila]
+          (-> fila (update ":budget" #(.intValue (Double. %)))
+                   (update ":gross" #(.intValue (Double. %)))
+                   (update ":runtime" #(Integer. %))
+                   (update ":score" #(Double. %))
+                   (update ":votes" #(Integer. %))
+                   (update ":year" #(Integer. %))
+          )
+        ) data)
+  )
+
 (defn read-csv [filepath]
   (let [contents (slurp filepath)]
-    (maps #"," contents)))
+    (parser-tipos (maps #"," contents)) ))
 
 (defn sum-key [data key]
-  (reduce + (map (fn [entry] (+ (parse-int (get entry key)))) data))
+  (reduce + (map (fn [entry] (+  (get entry key))) data))
+)
+
+(defn columns [data]
+  (keys (first data))
 )
 
 (defn calculate-average-key [data key]
@@ -48,7 +64,16 @@
   (map (fn [x] (list (first x) (count (second x)))) (group-by (fn [movie] (get movie key)) data))
 )
 
+(defn print-col-types [data]
+  (doseq [keyval (first data)] (println (key keyval) "type: "(type (val keyval)) ))
+)
 
+;(derive :types/float ::numero)
+;(derive :types/integer ::numero)
+;(derive :types/string  ::cadena)
+
+;(defmulti obtenerInfoColumna (fn [data key])
+;)
 
 (def filepath "resources/movies_1.csv")
 (def stored-data (read-csv filepath))
@@ -62,12 +87,15 @@
 (def cv-company (count-values stored-data ":company"))
 (def maxVotes (calculate-max-votes stored-data))
 
+
 (defn -main [& args]
   ;(println stored-data)
+  (print-col-types stored-data)
   (println "promedio de runtime:" average-runtime)
   (println "promedio de budget:" average-budget)
-  (println "count-values genre:" (count-values stored-data ":genre") )
-  (println "count-values company:" (count-values stored-data ":company") )
+  (println "count-values genre:" cv-genre )
+
+  ;(println "count-values company:" (count-values stored-data ":company") )
 
   ;(println maxVotes)
   (shutdown-agents)
